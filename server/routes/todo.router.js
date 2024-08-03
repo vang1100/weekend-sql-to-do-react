7f32b61a6f72c2426870d0486459cc37bb620af9
Padee Vang <vangpadee11@gmail.com>
2024-08-03 18:41:15 -0500

    created post, receiving internal server error

--- server/routes/todo.router.js
+++ server/routes/todo.router.js
@@ -23,7 +23,32 @@ router.get('/', function(req, res) {
     res.sendStatus(500);
   })
 });
-// POST
+// POST - to add data to DB
+
+router.post('/', (req, res) => {
+   // console.log('req.body', req.body);
+
+    const item = req.body.item;
+    const popular = req.body.popular;
+    const price = req.body.price;
+
+   const queryText = `
+                    INSERT INTO "cafe"
+                        ("item", "popular", "price")
+                    VALUES
+                        ($1, $2, $3);
+                    
+                    `;
+    pool.query(queryText, [item, popular, price])
+    .then(result => {
+        console.log('database insert resposnse successful', result);
+        res.sendStatus(201);
+    })
+    .catch(error => {
+        console.log('database insert response failed', error);
+        res.sendStatus(500);
+    });
+});
 
 // PUT
 
