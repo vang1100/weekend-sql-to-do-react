d3ebbe4a85dfd78b93be1e313fc23512f325bf5e
Padee Vang <vangpadee11@gmail.com>
2024-08-03 11:34:15 -0500

    added database sql

---
+++ cafe.sql
@@ -0,0 +1,17 @@
+CREATE TABLE "cafe" (
+	"id" SERIAL PRIMARY KEY,
+	"item" VARCHAR (100) NOT NULL,
+	"popular" BOOLEAN,
+	"price" INTEGER
+);
+INSERT INTO "cafe" 
+
+	("item", "popular", "price") 
+VALUES 
+	('croissant',true, 4.00 ),
+	('latte', true, 3.),
+	('cinnamon bagel', true, 4),
+	('iced lemonade', false, 3.50),
+	('strawberry cake', true, 7.50),
+	('matcha tea', true, 5.50),
+	('egg tart', false, 3.25);
