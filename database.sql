13edc16202b35b26a54bf085fb22e5db21766ab6
Padee Vang <vangpadee11@gmail.com>
2024-08-12 19:36:41 -0500

    updated database

--- database.sql
+++ database.sql
@@ -1,17 +1,17 @@
 CREATE TABLE "cafe" (
 	"id" SERIAL PRIMARY KEY,
 	"item" VARCHAR (100) NOT NULL,
-	"popular" BOOLEAN,
+	"popular" BOOLEAN DEFAULT FALSE,
 	"price" DECIMAL
 );
 INSERT INTO "cafe" 
 
-	("item", "popular", "price") 
+	("item", "price") 
 VALUES 
-	('croissant',true, 4.00 ),
-	('latte', true, 3.),
-	('cinnamon bagel', true, 4),
-	('iced lemonade', false, 3.50),
-	('strawberry cake', true, 7.50),
-	('matcha tea', true, 5.50),
-	('egg tart', false, 3.25);
+	('croissant',4.00 ),
+	('latte', 3.),
+	('cinnamon bagel', 4),
+	('iced lemonade',  3.50),
+	('strawberry cake', 7.50),
+	('matcha tea', 5.50),
+	('egg tart', 3.25);
