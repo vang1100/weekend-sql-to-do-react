41b3c151b3cee566b9ec612fad549591e0162755
Padee Vang <vangpadee11@gmail.com>
2024-08-12 19:34:46 -0500

    testing cafeArray.map()

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,10 +1,57 @@
 import {useEffect, useState} from 'react';
+import axios from 'axios';
 
 function App () {
+
+  let [cafeArray, setCafeArray] = useState([]);
+
+
+  useEffect(() => {
+    grabCafeItems();
+  }, [])
+
+  // GET CAFE ITEMS
+
+  const grabCafeItems = () => {
+    axios.get('/api/todo')
+      .then((response) => {
+        console.log(response.data);
+        setCafeArray(response.data);
+      })
+      .catch((error) => {
+        console.log(error)
+      })
+  }
+
+  // POST CAFE ITEMS
+
+  // DELETE CAFE ITEMS
+
+  // PUT CAFE ITEMS
   
   return (
     <div>
-      <h1>TO DO APP</h1>
+      <h1>CAFE MENU</h1>
+
+      tesing jsonstringify:
+      <ul>
+      {JSON.stringify(cafeArray)}
+      </ul>
+
+      tesing .map method:
+      <ul>
+        {cafeArray.map(
+          function(cafe) {
+            return (
+              <li key={cafe.id}>
+                {cafe.item} 
+                {cafe.popular} 
+                {cafe.price}
+              </li>
+            )
+          }
+        )}
+      </ul>
     </div>
   );
 
