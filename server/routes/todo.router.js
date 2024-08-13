fc566e48d8601305534f71e61806992e54521603
Padee Vang <vangpadee11@gmail.com>
2024-08-12 19:13:25 -0500

    router put created, successful in postman

--- server/routes/todo.router.js
+++ server/routes/todo.router.js
@@ -52,6 +52,24 @@ router.post('/', (req, res) => {
 
 // PUT
 
+router.put('/toggle/:id', (req, res) => {
+  let { id } = req.params;
+  const sqlText = `
+      UPDATE "cafe" SET "popular" = true
+      WHERE "id" = $1;
+  `;
+  pool.query(sqlText, [id])
+      .then((result) => {
+          console.log(`Got stuff back from the database`, result);
+          res.sendStatus(201);
+      })
+      .catch((error) => {
+          console.log(`Error making database query ${sqlText}`, error);
+          res.sendStatus(500); // Good server always responds
+      })
+});
+
+
 // DELETE
 
 router.delete('/:id', (req, res) => {
